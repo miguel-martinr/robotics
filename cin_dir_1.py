52ddb08232624d654b3c6e1944b85a05d49a5550
Miguel Martín <alu0101209777@ull.edu.es>
2021-10-31 21:55:44 +0000

    first attempt

--- cin_dir_1.py
+++ cin_dir_1.py
@@ -93,34 +93,42 @@ def matriz_T(d,theta,a,alpha):
 
 plt.ion() # Modo interactivo
 # Introducción de los valores de las articulaciones
-nvar=2 # Número de variables
+nvar=3 # Número de variables
 if len(sys.argv) != nvar+1:
   sys.exit('El número de articulaciones no es el correcto ('+str(nvar)+')')
 p=[float(i) for i in sys.argv[1:nvar+1]]
 
+th1 = p[0]
+l2 = p[1]
+th3 = p[2]
+
 # Parámetros D-H:
-#        1    2
-d  = [   0,   0]
-th = [p[0],p[1]]
-a  = [  10,   5]
-al = [   0,   0]
+#        1    2     3
+d  = [   5,  l2,    0]
+th = [ th1,  90, -th3]
+a  = [   0,   0,    2]
+al = [  90, -90,    0]
 
 # Orígenes para cada articulación
 o00=[0,0,0,1]
 o11=[0,0,0,1]
 o22=[0,0,0,1]
+o33=[0,0,0,1]
 
 # Cálculo matrices transformación
 T01=matriz_T(d[0],th[0],a[0],al[0])
 T12=matriz_T(d[1],th[1],a[1],al[1])
+T23=matriz_T(d[2],th[2],a[2],al[2])
 T02=np.dot(T01,T12)
+T03=np.dot(T02,T23)
 
 # Transformación de cada articulación
 o10 =np.dot(T01, o11).tolist()
 o20 =np.dot(T02, o22).tolist()
+o30 =np.dot(T03, o33).tolist()
 
 # Mostrar resultado de la cinemática directa
-muestra_origenes([o00,o10,o20])
-muestra_robot   ([o00,o10,o20])
+muestra_origenes([o00,o10,o20,o30])
+muestra_robot   ([o00,o10,o20,o30])
 input()
 
