8cff339df8b3980504ce9766cd7738b135b38623
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-01 20:43:45 +0000

    t4 51 added

--- cin_dir_1.py
+++ cin_dir_1.py
@@ -31,7 +31,7 @@ def ramal(I,prev=[],base=0):
         O.append(prev)
   return O
 
-def muestra_robot(O,ef=[]):
+def  muestra_robot(O,ef=[]):
   # Pinta en 3D
   OR = ramal(O)
   OT = np.array(OR).T
@@ -93,7 +93,7 @@ def matriz_T(d,theta,a,alpha):
 
 plt.ion() # Modo interactivo
 # Introducción de los valores de las articulaciones
-nvar=3 # Número de variables
+nvar=5 # Número de variables
 if len(sys.argv) != nvar+1:
   sys.exit('El número de articulaciones no es el correcto ('+str(nvar)+')')
 p=[float(i) for i in sys.argv[1:nvar+1]]
@@ -101,37 +101,47 @@ p=[float(i) for i in sys.argv[1:nvar+1]]
 l1 = p[0]
 th2 = p[1]
 l3 = p[2]
+th4 = p[3]
+th5 = p[4]
 
 # Parámetros D-H:
-#        01         11'   1'2   23
-d  = [   l1,         0,     0,  l3]
-th = [  -90,  th2 + 90,    90,   0]
-a  = [    0,         5,     0,   0]
-al = [    0,         0,    90,   0]
+#        01         11'   1'2   23          34  451
+d  = [   l1,         0,     0,  l3,          0,   0]
+th = [  -90,  th2 + 90,    90,   0,   th4 + 90,   th5]
+a  = [    0,         5,     0,   0,          0,   2]
+al = [    0,         0,    90,   0,         90,   90]
 
 # Orígenes para cada articulación
 o00=[0,0,0,1]
 o11=[0,0,0,1]
 o22=[0,0,0,1]
 o33=[0,0,0,1]
+o44=[0,0,0,1]
+o5151=[0,0,0,1]
 
 # Cálculo matrices transformación
 T01=matriz_T(d[0],th[0],a[0],al[0])
 T11p=matriz_T(d[1],th[1],a[1],al[1])
 T1p2=matriz_T(d[2],th[2],a[2],al[2])
 T23=matriz_T(d[3],th[3],a[3],al[3])
+T34=matriz_T(d[4],th[4],a[4],al[4])
+T451=matriz_T(d[5],th[5],a[5],al[5])
 
 T01p = np.dot(T01, T11p)
 T02 = np.dot(T01p, T1p2)
 T03 = np.dot(T02, T23)
+T04 = np.dot(T03, T34)
+T051 = np.dot(T04, T451)
 
 # Transformación de cada articulación
 o10 =np.dot(T01, o11).tolist()
 o20 =np.dot(T02, o22).tolist()
 o30 = np.dot(T03, o33).tolist()
+o40 = np.dot(T04, o44).tolist()
+o510 = np.dot(T051, o5151).tolist()
 
 # Mostrar resultado de la cinemática directa
-muestra_origenes([o00,o10,o20,o30])
-muestra_robot   ([o00,o10,o20,o30])
+muestra_origenes([o00,o10,o20,o30,o40,o510])
+muestra_robot   ([o00,o10,o20,o30,o40,o510])
 input()
 
